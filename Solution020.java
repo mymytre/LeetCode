4c15700624224cf34a9ff48e1701600f3872a8f9
mymytre <sasha.suliz@gmail.com>
2022-06-08 18:02:06 +0200

    Test cases

--- Solution020.java
+++ Solution020.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution020 {
 
     public static void main(String[] args) {
-        
+        String test1 = "()";
+        String test2 = "()[]{}";
+        String test3 = "(]";
+
+        Solution020 solution020 = new Solution020();
+
+        List<String> testString = new ArrayList<>();
+        testString.addAll(List.of(test1, test2, test3));
+        for (int i = 0; i < testString.size(); i++) {
+            System.out.print("Test number " + i + ": ");
+            System.out.println(solution020.isValid(testString.get(i)));
+        }
+
     }
 
     public boolean isValid(String s) {
+        boolean flagReturn = true;
+
         
+
+
+        return flagReturn;
     }
 
 }
