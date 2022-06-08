8ecb5ab460872d3af1ef0d35133a406c31511f63
mymytre <sasha.suliz@gmail.com>
2022-06-08 17:11:11 +0200

    update

---
+++ Solution14.java.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+ import java.util.HashSet;
+ import java.util.List;
+ import java.util.Set;
+
+ public class Solution14 {
+     public String longestCommonPrefix(String[] strs) {
+         StringBuilder stringBuilder = new StringBuilder();
+         Set<Character> characterSet;
+         char tmp = '\0';
+         List<Integer> integerList = new ArrayList<>();
+         for (int i = 0; i < strs.length; i++) {
+             integerList.add(strs[i].length());
+         }
+         List<Integer> sorted = integerList
+                 .stream()
+                 .sorted()
+                 .toList();
+
+
+         for (int j = 0; j < sorted.get(0); j++) {
+             characterSet = new HashSet<>();
+             for (int i = 0; i < strs.length; i++) {
+                 characterSet.add(strs[i].charAt(j));
+                 tmp = strs[i].charAt(j);
+             }
+             if (characterSet.size() == 1) {
+                 stringBuilder.append(Character.valueOf(tmp));
+             } else {
+                 if(stringBuilder.isEmpty()) {
+                     stringBuilder = new StringBuilder("");
+                 }
+                 break;
+             }
+         }
+         return String.valueOf(stringBuilder);
+     }
+ } 
